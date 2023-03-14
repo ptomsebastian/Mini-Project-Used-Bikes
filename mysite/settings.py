8e3c3ab553be0ae934008926e525ed68b0e6995c
ptomsebastian <94066515+ptomsebastian@users.noreply.github.com>
2023-03-14 16:50:37 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -19,8 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7yka+j+9^#57q1hy9jiz=sl3p(py#k=lg*2^l5r3h25bi1tcjm'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -153,12 +152,6 @@ RAZOR_KEY_ID = 'rzp_test_RSZJtMVZELUXuf'
 RAZOR_KEY_SECRET = 'cJj9zs9Z50Zspr7pBUjr4MLX'
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ptomsebastian@gmail.com'
-EMAIL_HOST_PASSWORD ='hzohyluoeicdjmde'
 
 
 # Default primary key field type
